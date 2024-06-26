f00a5d4dc1f67fd334b3ac34ad5c8af9939c4ffe
DryTurtle391 <binayakway@gmail.com>
2024-06-26 09:02:31 +0530

    Added New Accomodation Page

---
+++ client/src/pages/PlacesPage.jsx
@@ -0,0 +1,171 @@
+import { Link, useParams } from "react-router-dom";
+
+export default function PlacesPage() {
+  const { action } = useParams();
+  return (
+    <div>
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary rounded-full pl-3 pr-5 py-2 text-white"
+            to={"/account/places/new"}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="size-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Add new place
+          </Link>
+        </div>
+      )}
+      {action === "new" && (
+        <div className="new-place">
+          <form>
+            <h2 className="text-xl mt-4 ">Title</h2>
+            <input
+              type="text"
+              className=""
+              placeholder="Title, for example: Greymore Villa"
+            />
+            <h2 className="text-xl mt-4 ">Address</h2>
+            <input type="text" placeholder="Address" />
+            <h2 className="text-xl mt-4 ">Photos</h2>
+            <div className="flex gap-2">
+              <input type="text" placeholder="Add using link...   .jpg" />
+              <button className="bg-gray-200 rounded-full px-4">
+                Add&nbsp;Photo
+              </button>
+            </div>
+            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              <button className=" flex justify-center items-center gap-2 border bg-transparent text-xl rounded-2xl p-8  text-gray-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.47 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1-1.06 1.06l-3.22-3.22V16.5a.75.75 0 0 1-1.5 0V4.81L8.03 8.03a.75.75 0 0 1-1.06-1.06l4.5-4.5ZM3 15.75a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Upload
+              </button>
+            </div>
+            <div>
+              <h2 className="text-xl mt-4 ">Description</h2>
+              <textarea
+                type="text"
+                placeholder="Describe the accomodation..."
+              />
+            </div>
+            <div>
+              <h2 className="text-xl mt-4 ">Perks</h2>
+              <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <label className="border rounded-2xl flex gap-2 p-3">
+                  <input type="checkbox" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z"
+                    />
+                  </svg>
+                  <span>Wifi</span>
+                </label>
+                <label className="border rounded-2xl flex gap-2 p-3">
+                  <input type="checkbox" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
+                    />
+                  </svg>
+                  <span>TV</span>
+                </label>
+                <label className="border rounded-2xl flex gap-2 p-3">
+                  <input type="checkbox" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+                    />
+                  </svg>
+                  <span>Free Parking Spot</span>
+                </label>
+                <label className="border rounded-2xl flex gap-2 p-3">
+                  <input type="checkbox" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
+                  <span>Pet friendly</span>
+                </label>
+                <label className="border rounded-2xl flex gap-2 p-3">
+                  <input type="checkbox" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+                    />
+                  </svg>
+                  <span>24x7 security</span>
+                </label>
+              </div>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
